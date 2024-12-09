1734424296938cb5763e86e34c19529a387b015b
KilicEness <eneskiliicc06@gmail.com>
2024-12-09 19:05:27 +0300

    fix

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
             <img
               src={BannerImg}
               alt=""
-              className="max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover"
+              className="w-full h-auto mx-auto max-w-[400px] sm:h-[350px] drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover"
             />
           </div>
 
